bb00ccae699424a263eb0cab2c8f1c62e237e425
Miklós Árpád <miklosarpad2007@gmail.com>
2020-03-04 19:04:42 +0100

    service-ek hozzáadása a controllerhez

--- ASZTALI/ingatlan_centrum/ingatlan_centrum/ViewController/FormMenu.cs
+++ ASZTALI/ingatlan_centrum/ingatlan_centrum/ViewController/FormMenu.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using IngatlanCentrum.Service;
 using System.Windows.Forms;
 
 namespace IngatlanCentrum.ViewController
 {
     public partial class FormMenu : Form
     {
+        private EladoService eladoService;
+        private HirdetesService hirdetesService;
+        private IngatlanAllapotService ingatlanAllapotService;
+        private IngatlanKategoriaService ingatlanKategoriaService;
+        private IngatlanService ingatlanService;
+        private TelepulesService telepulesService;
+        private UgynokJogosultsagService ugynokJogosultsagService;
+        private UgynokService ugynokService;
+
         public FormMenu()
         {
             InitializeComponent();
+
+            eladoService = new EladoService();
+            hirdetesService = new HirdetesService();
+            ingatlanAllapotService = new IngatlanAllapotService();
+            ingatlanKategoriaService = new IngatlanKategoriaService();
+            ingatlanService = new IngatlanService();
+            telepulesService = new TelepulesService();
+            ugynokJogosultsagService = new UgynokJogosultsagService();
+            ugynokService = new UgynokService();
         }
     }
 }
