4b0fff38683a02bfc1f9682128563661da935173
Miklós Árpád <miklosarpad2007@gmail.com>
2020-03-11 13:31:06 +0100

    ügynök validációk

--- ASZTALI/ingatlan_centrum/ingatlan_centrum/Validation/UgynokValidator.cs
+++ ASZTALI/ingatlan_centrum/ingatlan_centrum/Validation/UgynokValidator.cs
@@ -7,6 +7,41 @@ namespace IngatlanCentrum.Validation
     {
         public static void Validate(Ugynok ugynok)
         {
+            if (UresE(ugynok.Id))
+            {
+                throw new UgynokException("Ügynök azonosítója nem lehet üres!");
+            }
+
+            if (TartalmazEKisbetut(ugynok.Id))
+            {
+                throw new UgynokException("Ügynök azonosítója nem tartalmazhat kisbetűt!");
+            }
+
+            if (!TartalmazESzamot(ugynok.Id))
+            {
+                throw new UgynokException("Ügynök azonosítójának tartalmaznia kell számot!");
+            }
+
+            if (MeghatarozottSzamuKaraktertTartalmazE(ugynok.Id, 6))
+            {
+                throw new UgynokException("Ügynök azonosítójának 6 karaktert kell tartalmazzon!");
+            }
+
+            if (UresE(ugynok.Vezeteknev) || UresE(ugynok.Keresztnev))
+            {
+                throw new UgynokException("Ügynök vezeték- vagy keresztneve nem lehet üres!");
+            }
+
+            if (UresE(ugynok.Telefonszam))
+            {
+                throw new UgynokException("Ügynök telefonszáma nem lehet üres!");
+            }
+
+            if (UresE(ugynok.Jogosultsag))
+            {
+                throw new UgynokException("Ügynök jogosultsága nem lehet üres!");
+            }
+
             if (UgynokNeveKisbetuvelKezdodik(ugynok.Vezeteknev) || UgynokNeveKisbetuvelKezdodik(ugynok.Keresztnev))
             {
                 throw new UgynokException("Ügynök vezeték- vagy keresztneve nem kezdődhet kisbetűvel!");
@@ -15,6 +50,11 @@ namespace IngatlanCentrum.Validation
             if (UgynokNeveSzamotTartalmaz(ugynok.Vezeteknev) || UgynokNeveSzamotTartalmaz(ugynok.Keresztnev))
             {
                 throw new UgynokException("Ügynök vezeték- vagy keresztneve nem tartalmazhat számot!");
+            }
+
+            if (TartalmazEBetut(ugynok.Telefonszam))
+            {
+                throw new UgynokException("Ügynök telefonszáma nem tartalmazhat betűt!");
             }
         }
 
@@ -36,6 +76,65 @@ namespace IngatlanCentrum.Validation
                 {
                     return true;
                 }
+            }
+
+            return false;
+        }
+
+        private static bool UresE(string bemenet)
+        {
+            if (string.IsNullOrEmpty(bemenet))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool TartalmazESzamot(string bemenet)
+        {
+            foreach (char karakter in bemenet)
+            {
+                if (char.IsDigit(karakter))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TartalmazEBetut(string bemenet)
+        {
+            foreach (char karakter in bemenet)
+            {
+                if (char.IsLetter(karakter))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TartalmazEKisbetut(string bemenet)
+        {
+            foreach (char karakter in bemenet)
+            {
+                if (char.IsLower(karakter))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool MeghatarozottSzamuKaraktertTartalmazE(string bemenet, int katakterekSzama)
+        {
+            if (bemenet.Length > katakterekSzama)
+            {
+                return true;
             }
 
             return false;
