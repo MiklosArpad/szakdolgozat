dad4124dffa83aa20bf52210ab818755dd97908f
MiklosArpad <miklosarpad2007@gmail.com>
2020-04-01 22:00:20 +0200

    kapcsolat oldal egybe rakása.

--- WEB/kapcsolat.php
+++ WEB/kapcsolat.php
@@ -8,11 +8,46 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <?php
-                    require_once 'html/navbar.html';
-                    require_once 'html/kapcsolat.html';
-                    require_once 'html/footer.php';
-                    ?>
+                    <?php require_once 'html/navbar.html'; ?>
+                    <table id="tablo">
+                        <thead>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <img src="img/szervettibor.jpg">
+                                    <h4>Szervét Tibor</h4>
+                                    <h5>Irodavezető</h5>
+                                    <h6>Szeged</h6>
+
+                                </td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="img/kisramona.jpg">
+                                    <h4>Kiss Ramóna</h4>
+                                    <h5>Értékesítő</h5>
+                                    <h6>Szeged</h6>
+                                </td>
+                                <td>
+                                    <img src="img/pindrochcsaba.jpg">
+                                    <h4>Pindroch Csaba</h4>
+                                    <h5>Értékesítő</h5>
+                                    <h6>Szeged</h6>
+
+                                </td>
+                                <td>
+                                    <img src="img/hevergabor.jpg">
+                                    <h4>Hevér Gábor</h4>
+                                    <h5>Értékesítő</h5>
+                                    <h6>Szeged</h6>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <?php require_once 'html/footer.php'; ?>
                 </div>
             </div>
         </div>
