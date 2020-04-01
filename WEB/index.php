d998707be3059edb6d63ce677991ea7318369df0
MiklosArpad <miklosarpad2007@gmail.com>
2020-04-01 21:58:27 +0200

    főoldal összeállítása

--- WEB/index.php
+++ WEB/index.php
@@ -8,12 +8,52 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <?php
-                    require_once 'html/navbar.html';
-                    require_once 'html/parallax.html';
-                    require_once 'html/impresszum.html';
-                    require_once 'html/footer.php';
-                    ?>
+                    <?php require_once 'html/navbar.html'; ?>
+                    <div class="kep1">
+                        <div class="kepszoveg">
+                            <span>IngatlanCentrum</span>
+                        </div>
+                    </div>
+                    <section class="sotet">
+                        <table id="hirek" class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>CSOK 10 +10</td>
+                                    <td><i class="fa">&#xf153;</i></td>
+                                </tr>
+                                <tr>
+                                    <td>Hitel</td>
+                                    <td>%</td>
+                                </tr>
+                                <tr>
+                                    <td>Jogi tanácsadás</td>
+                                    <td><i class='fa'>&#xf24e;</i></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <div class="kep2">
+                        <div class="kepszoveg">
+                            <span>Keresse meg álmai ingatlanát!</span>
+                        </div>
+                    </div>
+                    <div id="impresszum">
+                        <h3>Impresszum</h3>
+                        <h4>Irodavezető: Szervét Tibor</h4>
+                        <h4>
+                            &#9743; Telefon: 06-30-598-1778 <br>
+                            &#9993; E-mail: info@ingatlancentrum.hu<br>
+                            <i class="material-icons">&#xe55f;</i>Cím: Szeged, Gutenberg u. 11, 6722<br>
+                        </h4>
+                        <h4>Nyitvatartás:H-P: 09.00 - 18.00 (ebédszünet: 12.00 - 13.00)</h4>
+                    </div>
+                    <?php require_once 'html/footer.php'; ?>
                 </div>
             </div>
         </div>
